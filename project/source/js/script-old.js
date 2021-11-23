0b91155108e6ebfa1c610c0b23d4f004ff395340
zvdmail <sabirova.es@zv.digital>
2021-11-23 17:19:23 +0300

    new js

---
+++ project/source/js/script-old.js
@@ -0,0 +1,137 @@
+const messageSuccess = document.querySelector('.success');
+const messageError = document.querySelector('.error');
+const button = document.querySelector('.form__button');
+var fieldName = document.querySelector('.name');
+var fieldMail = document.querySelector('.mail');
+const fieldСonditions = document.querySelector('.conditions');
+const currentLocation = window.location;
+
+
+var mailValue = document.getElementById('field-mail').value;
+var form = document.querySelector('.form');
+const SHOW_TIME = 5000;
+
+var arr1 = document.location.href.split('?');
+var ticket = '';
+if (typeof arr1[1] != 'undefined') {
+  arr2 = (arr1[1]).split('&');
+  for (var i in arr2) {
+    var get_param = (arr2[i]).split('=');
+    if (get_param[0] == 'direct-crm-ticket') ticket = get_param[1];
+  }
+}
+
+// Показ сообщения об успешной отправке
+const showMessageSuccess = () => {
+  messageSuccess.classList.remove('visually-hidden');
+
+  setTimeout(() => {
+    messageSuccess.classList.add('visually-hidden');
+    }, SHOW_TIME);
+};
+
+
+// Показ сообщения об ошибке отправки
+const showMessageError = () => {
+
+  messageError.classList.remove('visually-hidden');
+
+  setTimeout(() => {
+    messageError.classList.add('visually-hidden');
+    }, SHOW_TIME);
+};
+
+
+// Код вызова для передачи адреса почты в Mindbox
+const wrapperMindbox = () => {
+    mindbox("async", {
+      operation: "LandingEmail",
+      data: {
+        customer: {
+          authenticationTicket: ticket,
+          email: fieldMail,
+          subscriptions: [
+            {
+              pointOfContact: "Email",
+              isSubscribed: "true"
+            }
+          ]
+        }
+      },
+      onSuccess: function(response) {
+        console.log("In success function");
+        console.log(response);
+        showMessageSuccess();
+      },
+      onError: function(error){
+        console.log("In error function");
+        console.log(error);
+        showMessageError();
+      }
+    });
+    console.log("mindbox");
+  }
+
+
+// Слушатель-2
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('form_submitted');
+  if ((fieldName.checkValidity() === true) & (fieldMail.checkValidity() === true) & (fieldСonditions.checkValidity() === true) ) {
+    const formFields = new FormData(e.target);
+    const fieldMail = formFields.get('mail');
+    console.log("ticket: "+ticket);
+    console.log("email:"+ fieldMail);
+    wrapperMindbox(ticket, fieldMail);
+
+}
+});
+
+
+// // Слушатель нажатия кнопки отправки
+// button.addEventListener('click', () => {
+//   if ((fieldName.checkValidity() === true) & (fieldMail.checkValidity() === true) & (fieldСonditions.checkValidity() === true) ) {
+
+//     // wrapperMindbox(ticket);
+//   }
+// });
+
+
+
+// button.addEventListener('click', () => {
+//   if ((fieldName.checkValidity() === true) & (fieldMail.checkValidity() === true) & (fieldСonditions.checkValidity() === true)) {
+
+//     function wrapper (ticket) {
+//     mindbox("async", {
+//       operation: "LandingEmail",
+//       data: {
+//         customer: {
+//           authenticationTicket: ticket,
+//           email: "mail-test-ya123@yandex.ru",
+//           subscriptions: [
+//             {
+//               pointOfContact: "Email",
+//               isSubscribed: "true"
+//             }
+//           ]
+//         }
+//       },
+//       onSuccess: function () {
+//         messageError.classList.remove('visually-hidden');
+
+//         setTimeout(() => {
+//           messageError.classList.add('visually-hidden');
+//         }, SHOW_TIME);
+//       },
+
+//       onError: function (error) {
+//         messageSuccess.classList.remove('visually-hidden');
+
+//         setTimeout(() => {
+//           messageSuccess.classList.add('visually-hidden');
+//         }, SHOW_TIME);
+//       }
+//     });
+//   }
+// }
+// });
